00c4b1f81b5056929e3e50eb62969402f0f51b2f
hungpham2017 <phamx494@umn.edu>
2019-04-07 01:25:09 -0500

    Adding MoS2 example, rm Ni

--- examples/MoS2/mcu_run.py
+++ examples/MoS2/mcu_run.py
@@ -1,4 +1,8 @@
 import mcu
 run = mcu.VASP()
 label = 'Y-G-R-X-G'
+
+# Style 2
+run.plot_pband(style=2,lm=['Mo:d','S:p'],color=['#00ccff','#ff0000'],alpha=0.4,label=label,ylim=(-1.5,1.5),figsize=(8,6))
+# Style 3
 run.plot_pband(style=3,lm='pd',label=label,ylim=(-1.5,1.5),figsize=(8,6))
