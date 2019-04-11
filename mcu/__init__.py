c6a911b9df941e3d7c070a6f04eaaa39558865d1
hungpham2017 <phamx494@umn.edu>
2019-04-11 14:35:56 -0500

    cleaning

--- mcu/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = '1.0.0'
-from . import vasp
-
-
-VASP = vasp.main.VASP
-read_vasprun = vasp.read.vasprun
-read_OUTCAR = vasp.read.OUTCAR
-read_WAVEDER = vasp.utils.read_WAVEDER
-read_WAVEDERF = vasp.utils.read_WAVEDERF
