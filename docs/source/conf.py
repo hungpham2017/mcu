4de4f5d4e97a62a1daeeb7aee937ce03d75c2b4a
hungpham2017 <phamx494@umn.edu>
2019-04-14 23:48:36 -0500

    Customize theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': '#2980B9',
+    'style_nav_header_background': '#008ae6',
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
